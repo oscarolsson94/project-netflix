b0a17677dc96c0a9f814e95f514955d9fd9fae29
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-19 11:00:56 +0200

    cleanup and working on home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import HomeScreen from './Components/HomeScreen';
 
 function App() {
   return (
     <div className="app">
-      
+          <h1>test</h1>
+          <HomeScreen />
     </div>
   );
 }

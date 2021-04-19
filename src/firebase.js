d50174a931efc08215799d22be85aa6126411d72
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-19 10:39:24 +0200

    cleaning up project structure

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBVs7gpPwFUeBH7Uckh7vTuvGU37KDYQd4",
+    authDomain: "netflix-clone-5e3f0.firebaseapp.com",
+    projectId: "netflix-clone-5e3f0",
+    storageBucket: "netflix-clone-5e3f0.appspot.com",
+    messagingSenderId: "987915562734",
+    appId: "1:987915562734:web:669ddefe6e64ccd5668dff",
+    measurementId: "G-T34ETKZKTB"
+  };

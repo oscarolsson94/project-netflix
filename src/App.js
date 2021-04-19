4f4c8b2e8860e659ee8587af2b6d5e0a05d2686a
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-19 11:45:26 +0200

    grabbing pictures for navbar and styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import HomeScreen from './Components/HomeScreen';
 function App() {
   return (
     <div className="app">
-          <h1>test</h1>
           <HomeScreen />
     </div>
   );
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,7 +4,19 @@ import "./Nav.css";
 const Nav = () => {
     return (
         <div className="nav">
-            
+            <div className="nav__contents">
+                <img
+                    className="nav__logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
+                    alt="netflix logo"
+                />
+
+                <img
+                    className="nav__avatar"
+                    src="https://occ-0-285-41.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
+                    alt="netflix avatar"
+                />
+            </div>
         </div>
     )
 }

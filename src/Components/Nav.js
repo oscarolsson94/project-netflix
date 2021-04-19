835609bfc884214336b245cb573b849acbe7b2cb
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-19 11:55:44 +0200

    styling naV

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,7 +3,7 @@ import "./Nav.css";
 
 const Nav = () => {
     return (
-        <div className="nav">
+        <div className="nav nav__black">
             <div className="nav__contents">
                 <img
                     className="nav__logo"

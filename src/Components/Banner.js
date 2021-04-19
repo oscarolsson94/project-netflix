c5d7bb6c3b1eb6fe0f8236bb32ab8a964cf5d0a0
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-19 12:47:09 +0200

    truncate function for description

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -2,6 +2,11 @@ import React from 'react';
 import "./Banner.css";
 
 const Banner = () => {
+
+    const truncate = (string, n) => {
+        return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+    }
+
     return (
         <header className="banner"
             style={{
@@ -16,7 +21,7 @@ const Banner = () => {
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
                 </div>
-                <h1 className="banner__description">This is a test description</h1>
+                <h1 className="banner__description">{truncate(`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil provident error dolor vitae cum nemo, debitis, modi facilis deserunt minus expedita. Quos eos, aperiam provident vel mollitia sequi dolores soluta.`, 150) }</h1>
             </div>
 
             <div className="banner--fadeBottom" />

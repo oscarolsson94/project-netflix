fe0486ac1995b8ec23a17cd0d28b00a3dbb298da
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-19 12:16:20 +0200

    styling header

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -3,9 +3,18 @@ import "./Banner.css";
 
 const Banner = () => {
     return (
-        <div className="banner">
+        <header className="banner"
+            style={{
+            backgroundSize: "cover",
+            backgroundImage: `url("https://www.phoneworld.com.pk/wp-content/uploads/2020/10/seo-watch-free-link-preview.jpg")`,
+            backgroundPosition: "center center",
+            }} >
             
-        </div>
+            <div className="banner__contents">
+                <h1 className="banner__title">Movie Name</h1>
+            </div>
+            
+        </header>
     )
 }
 

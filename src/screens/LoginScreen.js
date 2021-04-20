891056e49171fc2d4df5523b0aae33d9a5d78a9f
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-20 10:32:38 +0200

    styling loginScreen component

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,7 +4,18 @@ import './LoginScreen.css';
 const LoginScreen = () => {
     return (
         <div className="loginScreen">
-            
+            <div className="loginScreen__background">
+                <img
+                    className="loginScreen__logo"
+                    src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+                    alt="netflix logo"
+                />
+                <button
+                    className="loginScreen__button">
+                    Sign In
+                </button>
+                
+            </div>
         </div>
     );
 }

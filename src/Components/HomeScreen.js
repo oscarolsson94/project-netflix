558012d0465d251d35ad6bf1caf4a6741bd304a3
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-20 08:42:18 +0200

    setting up Row.js and sending in props

--- src/Components/HomeScreen.js
+++ src/Components/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Banner from './Banner';
 import "./HomeScreen.css";
 import Nav from './Nav';
+import Row from './Row';
+import requests from '../Requests';
 
 const HomeScreen = () => {
     return (
@@ -10,7 +12,18 @@ const HomeScreen = () => {
 
             <Banner />
 
-            {/* Rows */}
+            <Row
+              title="NETFLIX ORIGINALS"
+              fetchUrl={requests.fetchNetflixOriginals}
+              isLargeRow={true}
+            />
+            <Row title="Trending Now" fetchUrl={ requests.fetchTrending } />
+            <Row title="Top Rated" fetchUrl={ requests.fetchTopRated } />
+            <Row title="Action Movies" fetchUrl={ requests.fetchActionMovies } />
+            <Row title="Comedy Movies" fetchUrl={ requests.fetchComedyMovies } />
+            <Row title="Horror Movies" fetchUrl={ requests.fetchHorrorMovies } />
+            <Row title="Romance Movies" fetchUrl={ requests.fetchRomanceMovies } />
+          <Row title="Documentaries" fetchUrl={ requests.fetchDocumentaries } />
         </div>
     );
 }

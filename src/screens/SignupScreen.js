c6d9b8c1a9c3eab169a61ed4f24f3f3c30df9aa7
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-20 13:22:10 +0200

    adding signin function and setting up useEffect

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -22,6 +22,15 @@ const SignupScreen = () => {
 
     const handleSignIn = (e) => {
         e.preventDefault();
+
+        auth.signInWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        ).then((authUser) => {
+            console.log(authUser);
+        }).catch(error => {
+            alert(error.message);
+        });
     };
 
     return (

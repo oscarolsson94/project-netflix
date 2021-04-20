570659070d492cf18814e767ea423e1416395c94
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-20 08:35:39 +0200

    using the dynamic movie data for banner

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -27,17 +27,17 @@ const Banner = () => {
         <header className="banner"
             style={{
             backgroundSize: "cover",
-            backgroundImage: `url("https://www.phoneworld.com.pk/wp-content/uploads/2020/10/seo-watch-free-link-preview.jpg")`,
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
             backgroundPosition: "center center",
             }} >
             
             <div className="banner__contents">
-                <h1 className="banner__title">Movie Name</h1>
+                <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
                 <div className="banner__buttons">
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
                 </div>
-                <h1 className="banner__description">{truncate(`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil provident error dolor vitae cum nemo, debitis, modi facilis deserunt minus expedita. Quos eos, aperiam provident vel mollitia sequi dolores soluta.`, 150) }</h1>
+                <h1 className="banner__description">{truncate(movie?.overview, 150) }</h1>
             </div>
 
             <div className="banner--fadeBottom" />

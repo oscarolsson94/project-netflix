a8aea35b9e1801a133a408eb6eec3c98a2b2ce45
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-20 14:48:24 +0200

    setting up selector and dispatching login/logout

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import HomeScreen from './screens/HomeScreen';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
 import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 
 function App() {
 
-    const user = null;
+    const user = useSelector(selectUser);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+            if (userAuth) {
+                dispatch(login({
+                    uid: userAuth.uid,
+                    email: userAuth.email,
+                }))
+            } else {
+                dispatch(logout)
+            }
 
         });
 

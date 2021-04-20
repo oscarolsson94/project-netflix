e2e190d07097f22eadddbbd5760c3b7f44fdaae6
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-20 12:55:04 +0200

    setting up onClicks

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -2,17 +2,26 @@ import React from 'react';
 import "./SignupScreen.css";
 
 const SignupScreen = () => {
+
+    const register = (e) => {
+        e.preventDefault();
+    }
+
+    const handleSignIn = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div className="signupScreen">
             <form>
                 <h1>Sign In</h1>
                 <input placeholder="Email" type="email" />
                 <input placeholder="Password" type="password" />
-                <button type="submit">Sign In</button>
+                <button type="submit" onClick={handleSignIn}>Sign In</button>
 
                 <h4>
                     <span className="signupScreen__gray">New to Netflix? </span>
-                    <span className="signupScreen__link">Sign Up now.</span>
+                    <span className="signupScreen__link" onClick={register}>Sign Up now.</span>
                 </h4>
             </form>
         </div>

37f6aedd25d3239b6f9370e83c3e2cee4d14c30d
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-20 13:00:32 +0200

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyBVs7gpPwFUeBH7Uckh7vTuvGU37KDYQd4",
     authDomain: "netflix-clone-5e3f0.firebaseapp.com",
@@ -6,4 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "987915562734",
     appId: "1:987915562734:web:669ddefe6e64ccd5668dff",
     measurementId: "G-T34ETKZKTB"
-  };
+};
+  
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth, db };

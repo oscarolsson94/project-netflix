acb264e95427841ecff8d6b1fece7c644fd68179
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-20 11:11:31 +0200

    conditional rendering on button click

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginScreen.css';
 
 const LoginScreen = () => {
+
+    const [signIn, setSignIn] = useState(false);
+
     return (
         <div className="loginScreen">
             <div className="loginScreen__background">
@@ -11,6 +14,7 @@ const LoginScreen = () => {
                     alt="netflix logo"
                 />
                 <button
+                    onClick={()=> setSignIn(true)}
                     className="loginScreen__button">
                     Sign In
                 </button>
@@ -19,22 +23,26 @@ const LoginScreen = () => {
             </div>
 
             <div className="loginScreen__body">
-                <>
-                    <h1>Unlimited films, TV programmes and more.</h1>
-                    <h2>Watch anywhere. Cancel at any time.</h2>
-                    <h3>Ready to watch? Enter your email to create or restart your membership</h3>
+                {signIn ? (
+                    <SignInScreen />
+                ) : (
+                    <>
+                        <h1>Unlimited films, TV programmes and more.</h1>
+                        <h2>Watch anywhere. Cancel at any time.</h2>
+                        <h3>Ready to watch? Enter your email to create or restart your membership</h3>
 
-                    <div className="loginScreen__input">
-                        <form>
-                            <input
-                                placeholder="Email address"
-                                type="email"
-                            />
-                            <button className="loginScreen__getStarted">GET STARTED</button>
-                        </form>
-                    </div>
+                        <div className="loginScreen__input">
+                            <form>
+                                <input
+                                    placeholder="Email address"
+                                    type="email"
+                                />
+                                <button onClick={()=> setSignIn(true)} className="loginScreen__getStarted">GET STARTED</button>
+                            </form>
+                        </div>
+                    </>    
+                )}
                 
-                </>
             </div>
         </div>
     );

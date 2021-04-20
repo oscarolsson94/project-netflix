6752929e880d6d49571c576f86401d5ddf827a01
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-20 15:10:38 +0200

    adding redirect to nav image &  profileScreen

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -27,6 +27,7 @@ const Nav = () => {
         <div className={`nav ${show && "nav__black"}`}>
             <div className="nav__contents">
                 <img
+                    onClick={() => history.push("/")}
                     className="nav__logo"
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
                     alt="netflix logo"
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './ProfileScreen.css';
+import Nav from '../Components/Nav';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
 
 const ProfileScreen = () => {
+
+    const user = useSelector(selectUser);
+
     return (
         <div className="profileScreen" >
-            
+            <Nav />
+            <div className="profileScreen__body">
+                <h1>Edit Profile</h1>
+                <div className="profileScreen__info">
+                    <img src="https://occ-0-285-41.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41" alt="netflix avatar" />
+                    <div className="profileScreen__details">
+                        <h2>{user.email}</h2>
+                        <div className="profileScreen__plans">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
